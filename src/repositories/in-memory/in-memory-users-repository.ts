4eac527a6019fd0cd9fe1224983020570af8fa25
bruno9800 <bruno9800m@gmail.com>
2023-07-08 17:09:02 -0300

    update in memory repositories

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,5 +1,6 @@
 import { Prisma, User } from '@prisma/client';
 import { UsersRepository } from '../users-repository';
+import { randomUUID } from 'crypto';
 
 export class InMemoryUsersRepository implements UsersRepository {
   async findById(id: string): Promise<User | null> {
@@ -10,7 +11,7 @@ export class InMemoryUsersRepository implements UsersRepository {
   public users: User[] = [];
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
